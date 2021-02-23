93bcbfe1b4ea09e98d1401846b76fb4088a24c78
Bilal BAig <bilalbaig587@gmail.com>
2021-02-23 11:00:35 -0600

    after step 3

--- iDine/itemRow.swift
+++ iDine/itemRow.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct itemRow: View {
+struct ItemRow: View {
+    let item: MenuItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image(item.thumbnailImage)
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("S\(item.price)")
+            }
+        }
     }
 }
 
 struct itemRow_Previews: PreviewProvider {
     static var previews: some View {
-        itemRow()
+        ItemRow(item: MenuItem.example)
     }
 }
