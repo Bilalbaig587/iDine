f4ed03ee7b620fbf379b1bbdb2b9ebdf4317b2a5
Bilal BAig <bilalbaig587@gmail.com>
2021-02-23 10:51:21 -0600

    after step 2

---
+++ iDine/itemRow.swift
@@ -0,0 +1,20 @@
+//
+//  itemRow.swift
+//  iDine
+//
+//  Created by Bilal Baig on 2/23/21.
+//
+
+import SwiftUI
+
+struct itemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct itemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        itemRow()
+    }
+}
